e07e218ddb31010d9000356a72efed68c92949a4
kajiyama <dev.kajiyama@gmail.com>
2018-02-12 01:01:12 +0900

    Add class discription comments

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 import webbrowser
 
 class Movie():
+    """Movie class for a popup that is displayed at site top.
+
+    Attributes:
+        title: A string indicating a movie title.
+        poster_image_url: A string indicating a movie thumbnail image url.
+        trailer_youtube_url: A string indicating a movie url.
+    """
+
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         """Set movie info."""
