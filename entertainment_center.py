84148afed0c4870515438e6087f722850d633df9
kajiyama <dev.kajiyama@gmail.com>
2018-02-12 01:13:31 +0900

    Fix long string to be short

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,8 +9,10 @@ toy_story = media.Movie(
 
 harry_potter = media.Movie(
     "Harry Potter and the Deathly Hallows",
-    "2010 fantasy film directed by David Yates and distributed by Warner Bros. ",
-    "https://upload.wikimedia.org/wikipedia/en/2/2d/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_1.jpg",
+    ("2010 fantasy film directed by David Yates "
+     "and distributed by Warner Bros."),
+    ("https://upload.wikimedia.org/wikipedia/en/2/2d/"
+     "Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_1.jpg"),
     "https://www.youtube.com/watch?v=9hXH0Ackz6w")
 
 fresh_tomatoes.open_movies_page([toy_story, harry_potter])
